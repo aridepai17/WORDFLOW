557054d1861f60f7a095f2832694407fd51ef193
aridepai17 <advaithdepai26@gmail.com>
2025-07-22 19:17:14 +0530

    feat(Challenge.jsx): Build the main challenge and learning interface
    A display area for the target word and its definition
    
    An input field for the user to type their answer
    
    A visual 'helper' that shows the length of the definition, guiding the user
    
    Action buttons for 'quit' and 'I forgot' to handle diffirent user scenarios
    
    Integration of 'ProgressBar' to show the user's progress through the current session
    
    Current version contains static and hardcoded UI mock up for layout and styling purposes

--- src/components/layouts/Challenge.jsx
+++ src/components/layouts/Challenge.jsx
@@ -1,5 +1,39 @@
+import ProgressBar from '../ProgressBar';
+
+
 export default function Challenge() {
+  const word = 'wordflow'
+  const definition = 'Progression of words'
+
+
+
+
+
   return (
-    <div></div>
+    <section id="challenge">
+      <h1>{word}</h1>
+      <p>{definition}</p>
+      <div className="helper">
+        <div>
+
+          {[...Array(definition.length).keys()].map((element, elementIdx) =>
+          { 
+            return (
+            <div key={elementIdx}></div>
+          )})}
+        </div>
+        <input type="text" placeholder="Enter the definition..." />
+      </div>
+
+      <div className="challenge-btns">
+        <button className="card-button-secondary">
+          <h6>Quit</h6>
+        </button>
+        <button className="card-button-primary">
+          <h6>I forgot</h6>
+        </button>
+      </div>
+      <ProgressBar />
+    </section>
   )
 }
