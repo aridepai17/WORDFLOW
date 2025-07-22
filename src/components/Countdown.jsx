b14a54623eff579b1fe5d8a5ed1029fde24aa4e8
aridepai17 <advaithdepai26@gmail.com>
2025-07-22 12:34:12 +0530

    feat(Countdown.jsx): Construct daily countdown and start task card
    A prominent header indicating the current challenge day
    
    A display for the time remaining in the current 24-hour cycle
    
    A counter for the number of words assigned for the day
    
    A 'Start' button to begin the learning session
    
    All displayed values are hardcoded and static in the current version for styling and development purpose

--- src/components/Countdown.jsx
+++ src/components/Countdown.jsx
@@ -1,5 +1,23 @@
 export default function Countdown() {
   return (
-    <div></div>
+    <div className="card countdown-card">
+      <h1 className="item-header">Day {1}</h1>
+      <div className="today-container">
+        <div>
+          <p>Time remaining</p>
+          <h3>12H 45M 56S</h3>
+        </div>
+
+        <div>
+          <p>Words for today</p>
+          <h3>16</h3>
+        </div>
+      </div>
+
+      <button className="start-task">
+        <h6>Start</h6>
+      </button>
+
+    </div>
   )
 }
