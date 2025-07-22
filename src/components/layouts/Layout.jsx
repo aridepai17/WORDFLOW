bed0d58ae5f090e91c308fc03d0ddbf2eb59834c
aridepai17 <advaithdepai26@gmail.com>
2025-07-22 23:34:28 +0530

    feat(Layout.jsx): Implement main application header and footer
    Established the main app layout with a consistent header and footer
    
    Renders child components dynamically within the 'main' element
    
    Added  static header with the 'WordFlow' application title
    
    Implemented a footer with creator attribution and a link to the GitHub repository

--- src/components/layouts/Layout.jsx
+++ src/components/layouts/Layout.jsx
@@ -1,5 +1,22 @@
-export default function Layout() {
+export default function Layout(props) {
+  console.log(props)
+  const { children } = props
   return (
-    <div></div>
+    <>
+      <header>
+        <h1 className="text-gradient">WordFlow</h1>
+      </header>
+      <main>
+        {children}
+      </main>
+      <footer>
+        <small>Created by</small>
+        <a target="_blank" href="https://github.com/aridepai17/WORDFLOW">
+          <img alt="pfp" src="https://avatars.githubusercontent.com/u/163564671?v=4"/>
+          <p>@aridepai17</p>
+          <i className="fa-brands fa-github"></i>
+        </a>
+      </footer>
+    </>
   )
 }
