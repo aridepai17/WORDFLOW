efacf551683e1e055308940d6fa76e11bafaeeed
aridepai17 <advaithdepai26@gmail.com>
2025-07-22 20:16:09 +0530

    feat(App.jsx): Implement root component with state and page routing
    1. State Management
    
     - Utilizes useState hook to manage two critical pieces fo global state
    
     - selectedPage: Controls which component/view is currently visible to the user
    
     - name: Stores the user's name after they complete the onboarding
    
    2. Client-Side Routing
    
     - Implements a simple but effective page routing system by mapping an index to a component with a pages object
    
     - The handleChangePage function provides a clean API for navigating between the 'Welcome', 'Dashboard', and 'Challenge' views
    
    3. User Persistence with LocalStorage
    
     - A useEffect hook runs on initial application to load to check for a username in localStorage
    
     - If a user is found, the app bypasses the 'Welcome' screen and navigates them directly to the 'Dashboard', creating a seamless return-user experience
    
    The 'handleCreateAccount' function saves the user's name to 'localStorage' upon creation

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,44 @@
-
+import Layout from "./components/layouts/Layout"
+import Welcome from "./components/layouts/Welcome"
+import Dashboard from "./components/layouts/Dashboard"
+import Challenge from "./components/layouts/Challenge"
+import { useState, useEffect } from "react"
 
 function App() {
+  const [selectedPage, setSelectedPage] = useState(0)
+  const [name, setName] = useState('')
+
+  function handleChangePage(pageIndex) {
+    setSelectedPage(pageIndex)
+  }
+
+  function handleCreateAccount() {
+    if (!name) { return }
+    localStorage.setItem('username', name)
+    handleChangePage(1)
+  }
+
+  useEffect(() => {
+    if (!localStorage) { return }
+    if (localStorage.getItem('username')) {
+      setName(localStorage.getItem('username'))
+      setSelectedPage(1)
+    }
+  }, [])
+
+  
+
+  const pages = {
+    0 : <Welcome handleCreateAccount={handleCreateAccount} username= 'hello world' name={name} setName={setName} />,
+    1 : <Dashboard />,
+    2 : <Challenge />
+  }
   
 
   return (
-    <div></div>
+    <Layout>
+      {pages[selectedPage]}
+    </Layout>
       
     
   )
