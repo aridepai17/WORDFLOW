aca6c382aa4f46bf2bda298610c7e003204a4a7b
aridepai17 <advaithdepai26@gmail.com>
2025-07-22 12:29:54 +0530

    feat(History.jsx): Build History card to display past attempts
    An 'empty state' message for new users, guiding them to start their first session
    
    A styled example of history list item, which will be used to display data like sesstion start dates or streaks
    
    Current version is static and hardcoded for styling and development purposes

--- src/components/History.jsx
+++ src/components/History.jsx
@@ -1,5 +1,20 @@
 export default function History() {
   return (
-    <div></div>
+    <div className="card history-card">
+      <h4>History</h4>
+      <p>You have no attempts! Press <b>Start</b> to begin 🙌</p>
+      <div className="history-list">
+        <div className="card-button-secondary">
+          <div>
+            <p>Started</p>
+            <h6>May 4 2021</h6>
+          </div>
+          <div>
+            <p>Streak</p>
+            <h6>56</h6>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
