8a1d415da2b2d1bb7516f19272e830a1856a5892
aridepai17 <advaithdepai26@gmail.com>
2025-07-23 00:28:57 +0530

    refactor(Stats.jsx): Make the component dynmaic and data-driven
    Refactored component from a static mockup to be fully dynamic
    
    Now accepts props ('name', 'day', 'attempts', 'PLAN') to receive application state
    
    Imports and utilizes utility functions ('calcLevel', 'calculateAccuracy', 'calculateNewWords')
    
    Calculates user statistics like level, words seen, and accuracy in real-time
    
    Passes calculated level and progress remainder props to the 'ProgressBar' component

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,35 +1,36 @@
-import ProgressBar from './ProgressBar'
+import ProgressBar from "./ProgressBar";
+import { calcLevel, calculateAccuracy, calculateNewWords } from "../utils";
 
+export default function Stats(props) {
+  const { name, day, attempts, PLAN } = props;
 
-export default function Stats(){
-  const name = 'R Pai'
-  const day = 16
+  const currLvl = calcLevel(day);
+  const flooredLvl = Math.floor(currLvl);
+  const remainder = (currLvl - flooredLvl) * 100;
 
   return (
     <div className="card stats-card">
       <div className="welcome-text">
         <h6>Welcome</h6>
-        <h4 className="text-large">
-          {name}
-        </h4>
+        <h4 className="text-large">{name}</h4>
       </div>
 
-        <div className="stats-column">
-          <div>
-            <p>Streak ⭐</p>
-            <h4>{day - 1}</h4>
-          </div>
-          <div>
-            <p>Words seen</p>
-            <h4>{101}</h4>
-          </div>
-          <div>
-            <p>Accuracy (%)</p>
-            <h4>{93.3.toFixed(2)}</h4>
-          </div>
+      <div className="stats-column">
+        <div>
+          <p>Streak ⭐</p>
+          <h4>{day - 1}</h4>
         </div>
-
-        <ProgressBar />
+        <div>
+          <p>Words seen</p>
+          <h4>{calculateNewWords(day - 1)}</h4>
+        </div>
+        <div>
+          <p>Accuracy (%)</p>
+          <h4>{(calculateAccuracy(attempts, day) * 100).toFixed(1)}</h4>
+        </div>
       </div>
-  )
+
+      <ProgressBar text={`lvl ${flooredLvl}`} remainder={remainder} />
+    </div>
+  );
 }
