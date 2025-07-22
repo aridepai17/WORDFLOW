2e6b5803b0a9ec39eabfa807c69bbf762f5770ff
aridepai17 <advaithdepai26@gmail.com>
2025-07-22 21:35:48 +0530

    chore(App.jsx): Minor change and tweaks

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,49 @@
-import Layout from "./components/layouts/Layout"
-import Welcome from "./components/layouts/Welcome"
-import Dashboard from "./components/layouts/Dashboard"
-import Challenge from "./components/layouts/Challenge"
-import { useState, useEffect } from "react"
+import Layout from "./components/layouts/Layout";
+import Welcome from "./components/layouts/Welcome";
+import Dashboard from "./components/layouts/Dashboard";
+import Challenge from "./components/layouts/Challenge";
+import { useState, useEffect } from "react";
 
 function App() {
-  const [selectedPage, setSelectedPage] = useState(0)
-  const [name, setName] = useState('')
+  const [selectedPage, setSelectedPage] = useState(0);
+  const [name, setName] = useState("");
 
   function handleChangePage(pageIndex) {
-    setSelectedPage(pageIndex)
+    setSelectedPage(pageIndex);
   }
 
   function handleCreateAccount() {
-    if (!name) { return }
-    localStorage.setItem('username', name)
-    handleChangePage(1)
+    if (!name) {
+      return;
+    }
+    localStorage.setItem("username", name);
+    handleChangePage(1);
   }
 
   useEffect(() => {
-    if (!localStorage) { return }
-    if (localStorage.getItem('username')) {
-      setName(localStorage.getItem('username'))
-      setSelectedPage(1)
+    if (!localStorage) {
+      return;
     }
-  }, [])
-
-  
+    if (localStorage.getItem("username")) {
+      setName(localStorage.getItem("username"));
+      setSelectedPage(1);
+    }
+  }, []);
 
   const pages = {
-    0 : <Welcome handleCreateAccount={handleCreateAccount} username= 'hello world' name={name} setName={setName} />,
-    1 : <Dashboard />,
-    2 : <Challenge />
-  }
-  
+    0: (
+      <Welcome
+        handleCreateAccount={handleCreateAccount}
+        username="hello world"
+        name={name}
+        setName={setName}
+      />
+    ),
+    1: <Dashboard name={name} />,
+    2: <Challenge />,
+  };
 
-  return (
-    <Layout>
-      {pages[selectedPage]}
-    </Layout>
-      
-    
-  )
+  return <Layout>{pages[selectedPage]}</Layout>;
 }
 
-export default App
+export default App;
