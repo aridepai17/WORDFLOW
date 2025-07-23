e206a07da24f4f3486109b6422eec6120f14c60f
aridepai17 <advaithdepai26@gmail.com>
2025-07-23 10:58:30 +0530

    refactor(History.jsx): Make the History component dynamic and render the history log
    Refactored component to accept the 'history' object as prop
    
    Dynamically maps over the 'history' object keys to render a list of all past attempts
    
    Formats the date for each history entry for a clean display

--- src/components/History.jsx
+++ src/components/History.jsx
@@ -1,20 +1,36 @@
-export default function History() {
+export default function History(props) {
+  const { history } = props;
+  const historyKeys = Object.keys(history);
   return (
     <div className="card history-card">
       <h4>History</h4>
-      <p>You have no attempts! Press <b>Start</b> to begin 🙌</p>
-      <div className="history-list">
-        <div className="card-button-secondary">
-          <div>
-            <p>Started</p>
-            <h6>May 4 2021</h6>
-          </div>
-          <div>
-            <p>Streak</p>
-            <h6>56</h6>
-          </div>
+      {historyKeys.length == 0 ? (
+        <p>
+          You have no attempts! Press <b>Start</b> to begin 🙌
+        </p>
+      ) : (
+        <div className="history-list">
+          {historyKeys.map((item, itemIdx) => {
+            const dateKey = new Date(item)
+              .toString()
+              .split(" ")
+              .slice(1, 4)
+              .join(" ");
+            return (
+              <div key={itemIdx} className="card-button-secondary">
+                <div>
+                  <p>Started</p>
+                  <h6>{dateKey}</h6>
+                </div>
+                <div>
+                  <p>Streak</p>
+                  <h6>{history[item]}</h6>
+                </div>
+              </div>
+            );
+          })}
         </div>
-      </div>
+      )}
     </div>
-  )
+  );
 }
